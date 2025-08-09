771061c1f06c2b07d3ac2e3e39647f7b6cea2db8
William Jackson <wcjaackson@gmail.com>
2025-08-09 16:22:58 +1000

    remove review layout + sublayout

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -1,9 +1,3 @@
-import Layout from "@/components/sublayout/sublayout";
-
 export default function ReviewsPage() {
-  return (
-    <Layout title="My Reviews">
-      <p className="text-gray-700">Here are your latest reviews...</p>
-    </Layout>
-  );
+  return <p className="text-gray-700">Here are your latest reviews...</p>;
 }
