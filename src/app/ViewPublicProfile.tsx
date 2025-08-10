16ad5f3a93cad2dc4ad761ea4d9bc91327910f48
William Jackson <wcjaackson@gmail.com>
2025-08-10 14:48:47 +1000

    remove unnecessary details

--- src/app/ViewPublicProfile.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useScore } from "@/components/score/ScoreContext";
-import Link from "next/dist/client/link";
-
-export default function ViewPublicProfile() {
-  const { userId } = useScore();
-  return <Link href={`/profile/${userId}`}>View Public Profile</Link>;
-}
